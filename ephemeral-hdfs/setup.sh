83bc694fb0141d794b5c1a51edd0ca665a8046c5
Andre Kuhnen <andre.kuhnen@holidaycheck.com>
2017-10-01 14:37:30 +0200

    stop hdfs and yarn before starting it

--- ephemeral-hdfs/setup.sh
+++ ephemeral-hdfs/setup.sh
@@ -37,7 +37,12 @@ case "$HADOOP_MAJOR_VERSION" in
     $EPHEMERAL_HDFS/sbin/start-dfs.sh
     ;;
   yarn) 
+    echo "Stoping  HDFS"
+    $EPHEMERAL_HDFS/sbin/stop-dfs.sh
+    echo "Starting HDFS"
     $EPHEMERAL_HDFS/sbin/start-dfs.sh
+    echo "Stoping  YARN"
+    $EPHEMERAL_HDFS/sbin/stop-yarn.sh
     echo "Starting YARN"
     $EPHEMERAL_HDFS/sbin/start-yarn.sh
     ;;
