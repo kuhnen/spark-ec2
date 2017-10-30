64f77b39e5c0a57375c59c3dfc869af8ced31487
Andre Kuhnen <andrekuhnen@gmail.com>
2017-10-30 14:52:48 +0100

    Update spark_ec2.py
    rstudio out

--- spark_ec2.py
+++ spark_ec2.py
@@ -845,8 +845,10 @@ def setup_cluster(conn, master_nodes, slave_nodes, opts, deploy_ssh_key):
             print(slave_address)
             ssh_write(slave_address, opts, ['tar', 'x'], dot_ssh_tar)
 
+#   modules = ['spark', 'ephemeral-hdfs', 'persistent-hdfs',
+#                'mapreduce', 'spark-standalone', 'tachyon', 'rstudio']
     modules = ['spark', 'ephemeral-hdfs', 'persistent-hdfs',
-               'mapreduce', 'spark-standalone', 'tachyon', 'rstudio']
+               'mapreduce', 'spark-standalone', 'tachyon']
 
     if opts.hadoop_major_version == "1":
         modules = list(filter(lambda x: x != "mapreduce", modules))
